a52195c493294e0e530e52bbc6f7974f92136127
chetub <chetan.byalihal@gmail.com>
2020-10-24 05:21:21 +0000

    Added Yocto Build

--- sync.sh
+++ sync.sh
@@ -1,1 +1,16 @@
-echo "sync"
+#!/bin/bash
+
+rm -rf yocto
+mkdir -p yocto
+cd yocto
+
+git clone git://git.yoctoproject.org/poky
+cd poky
+git checkout tags/yocto-3.1.3 -b my-yocto-3.1.3
+
+source oe-init-build-env
+
+bitbake core-image-sato
+
+
+
